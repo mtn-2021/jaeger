76faf841db396ed1c5f564ebafcb8a674aac8649
mtn-2020 <b17t2105c@edu.tottori-u.ac.jp>
2020-12-18 22:44:55 +0900

    fix

--- z-execute.sh
+++ z-execute.sh
@@ -1,5 +1,7 @@
-SPAN_STRAGE_TYPE=cassandra go run ./cmd/collector/main.go --cassandra.keyspace="logs" --cassandra.servers="10.0.2.15" &
-go run ./cmd/agent/main.go --reporter.grpc.host-port=10.0.2.15:14250 &
-SPAN_STRAGE_TYPE=cassandra go run -tags ui ./cmd/query/main.go --cassandra.keyspace="logs" --cassandra.servers="10.0.2.15" --query.ui-config=./z-ui-config.json 
+#$1 agent $2 server(s) .../",/"...
+
+SPAN_STRAGE_TYPE=cassandra go run ./cmd/collector/main.go --cassandra.keyspace="logs" --cassandra.servers="$2" &
+go run ./cmd/agent/main.go --reporter.grpc.host-port=$1:14250 &
+SPAN_STRAGE_TYPE=cassandra go run -tags ui ./cmd/query/main.go --cassandra.keyspace="logs" --cassandra.servers="$2" --query.ui-config=./z-ui-config.json 
 #--cassandra-archive.enabled --cassandra-archive.keyspace="archives" --cassandra-archive.servers="10.0.2.15"
 
--- z-init.sh
+++ z-init.sh
@@ -1,3 +1,4 @@
 git clone https://github.com/jaegertracing/jaeger-idl.git idl
 git clone https://github.com/mtn-2020/jaeger-ui.git jaeger-ui
+make install-tools
 make build-ui
