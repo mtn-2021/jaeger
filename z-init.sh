9e3bff35f71515140e8a996b3df1e65f4ea138d9
lab2703-PC\matsumotoN2020 <m21j4059z@edu.tottori-u.ac.jp>
2021-12-21 17:51:10 +0900

    update

--- z-init.sh
+++ z-init.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/jaegertracing/jaeger-idl.git idl
-git clone https://github.com/mtn-2020/jaeger-ui.git jaeger-ui
+git clone https://github.com/mtn-2021/jaeger-ui.git jaeger-ui
 make install-tools
 make build-ui
