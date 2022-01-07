b26d6bc08c80adef29d6241a632d3d1579834646
lab2703-PC\matsumotoN2020 <m21j4059z@edu.tottori-u.ac.jp>
2022-01-07 18:05:51 +0900

    a

--- z-init.sh
+++ z-init.sh
@@ -2,3 +2,4 @@ git clone https://github.com/jaegertracing/jaeger-idl.git idl
 git clone https://github.com/mtn-2021/jaeger-ui.git jaeger-ui
 make install-tools
 make build-ui
+yarn add react-chartjs-2@3.0.3
