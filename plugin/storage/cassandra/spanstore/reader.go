1aea5140f8cb6745b514aba1994111e8b32b4d26
lab2703-PC\matsumotoN2020 <m21j4059z@edu.tottori-u.ac.jp>
2022-01-07 20:09:42 +0900

    a

--- plugin/storage/cassandra/spanstore/reader.go
+++ plugin/storage/cassandra/spanstore/reader.go
@@ -447,6 +447,7 @@ func (s *SpanReader) GetNodes(ctx context.Context) (map[string]spanstore.NodeSer
 
 func (s *SpanReader) GetNodeStatus(ctx context.Context,query *spanstore.RequestToNodeQuery) ([]spanstore.DetailLogs, error) {
 	fmt.Println("in status reader")
+	fmt.Println(query.Node)
 	fmt.Println(model.TimeAsEpochMicroseconds(query.StartTimeMin))
 	fmt.Println(model.TimeAsEpochMicroseconds(query.StartTimeMax))
 
